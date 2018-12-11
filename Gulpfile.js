f85f26de951baf79c1a7c4e8cae560b99531b2f5
Chad Ohl <chadohl@gmail.com>
2018-12-10 16:37:12 -0800

    Updated nav styles; Added fontawesome;

--- Gulpfile.js
+++ Gulpfile.js
@@ -3,10 +3,18 @@ var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function() {
-    gulp.src('stylesheets/sass/**/*.scss')
+    gulp.src('stylesheets/sass/main.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('stylesheets/css/'))
         .pipe(browserSync.stream());
+});
+
+gulp.task('icons', function() {
+    gulp.src('node_modules/@fortawesome/fontawesome-free-webfonts/scss/**.*')
+        .pipe(gulp.dest('stylesheets/sass/fontawesome/scss'));
+
+    gulp.src('node_modules/@fortawesome/fontawesome-free-webfonts/webfonts/**.*')
+        .pipe(gulp.dest('fonts/fontawesome/webfonts'));
 });
 
 
@@ -17,7 +25,7 @@ gulp.task('serve', ['sass'], function() {
         server: "."
     });
 
-    gulp.watch("stylesheets/sass/*.scss", ['sass']);
+    gulp.watch("stylesheets/sass/**/*.scss", ['sass']);
     gulp.watch("*.html").on('change', browserSync.reload);
 });
 
